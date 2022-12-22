5472bf17c4d698868b93ad7088e2bd8ad4920230
Lukasz <ufabia03@gmail.com>
2022-12-22 10:51:47 +0100

    git commit

--- src/Test.java
+++ src/Test.java
@@ -3,5 +3,6 @@ import Backend.NumbersSystem;
 public class Test {
     public static void main(String[] args) {
         NumbersSystem a = new NumbersSystem();
+        System.out.println(a.binDec("XD"));
     }
 }
